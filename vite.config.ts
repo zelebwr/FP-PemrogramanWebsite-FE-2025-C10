e89a3a7e8b103049f1bbc40cbef5abff897ef290
samuelyuma <samuelyuma.117@gmail.com>
2025-11-26 11:01:01 +0700

    fix: allow host for frontend (production)

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    allowedHosts: ["http://dev-wordit.it-its.id"],
+    allowedHosts: ["http://dev-wordit.it-its.id", "http://wordit.it-its.id"],
   },
   preview: {
     host: true,
     port: 3001,
-    allowedHosts: ["http://dev-wordit.it-its.id"],
+    allowedHosts: ["http://dev-wordit.it-its.id", "http://wordit.it-its.id"],
   },
 });
